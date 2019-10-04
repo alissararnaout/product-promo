c62464c2f8b3d38eb46227e9d53fda58139a392d
Alissar Arnaout <Alissar@Alissars-MacBook-Air.local>
2019-10-03 21:59:27 -0400

    html, css, linking done

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path'); // path lets us navigate the file system / folders
+
+// heroku assigns a port when it deploys via the process (environment variables - coming soon)
+// locally this will run @ port 3000; remotely it'll run wherever heroku tells it to run
+const port = process.env.PORT || 3000; // a double pipe -> || means "or"
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+// a forward slash is the home route (same as index.html)
+app.get('/', (req, res) => {
+  console.log('at the home route');
+  res.sendFile(path.join(__dirname + '/product/index.html'));
+  // this builds localhost:3000/views/index.html
+})
+
+app.get('/buy', (req, res) => {
+  console.log('at the buy route');
+  res.sendFile(path.join(__dirname + '/product/buy.html'));
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
